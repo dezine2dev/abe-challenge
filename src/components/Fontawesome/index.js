72ac5e5163a6221bdef02480f0a24fba0d9c086e
Isa N <isanoguchi91@gmail.com>
2020-04-12 08:21:26 -0500

    update(components): tweak Fontawesome component

--- src/components/Fontawesome/index.js
+++ src/components/Fontawesome/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { config, library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import {
+  faSearch,
+  faChevronRight,
+  faPlusCircle,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons';
 
 import '@fortawesome/fontawesome-svg-core/styles.css';
+import './Fontawesome.scss';
 
 config.autoAddCss = false;
-library.add(faSearch);
+library.add(faSearch, faChevronRight, faPlusCircle, faPlus);
 
-export default ({ ...rest }) => {
-  return <FontAwesomeIcon {...rest} />;
+export default ({ color, ...rest }) => {
+  return <FontAwesomeIcon className="fontawesome__icon" color={color} {...rest} />;
 };
